0e57f090a616bcd59a31fcf6b73fa1fef3aa81aa
NouhRastanawi <noahrastdark@gmail.com>
2022-04-24 14:25:38 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function writeCards(arr) {
+    let cards = [];
+        for (let i = 0; i < arr.length; i++) {
+            const newNames = [];
+            cards.push(`Thank you, ${arr[i]}, for the wonderful surprise gift!`);
+        }
+    return cards;
+}
+
+function countDown(x) {
+    while (x > 0) {
+        console.log(x);
+        x -= 1;
+    }
+    console.log(x);
+}
+
+console.log(countDown(7));
